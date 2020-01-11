6e657e00197893d2d45b26d51dcb80c77366949d
Kinga <gniedziejko.kinga@gmail.com>
2020-01-11 23:37:46 +0100

    Lokalna db

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// ------------------------- połączenie z serwera do bd -----------------
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'sql111.epizy.com',
+// 	'username' => 'epiz_24917383',
+// 	'password' => 'C79HAhQud1Gr',
+// 	'database' => 'epiz_24917383_DBplacowka',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'sql111.epizy.com',
-	'username' => 'epiz_24917383',
-	'password' => 'C79HAhQud1Gr',
-	'database' => 'epiz_24917383_DBplacowka',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'placowka_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
