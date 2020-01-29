6ea72a980c2804116ca7b4d46bbc55bdec7408d2
igahupalo <56322245+igahupalo@users.noreply.github.com>
2020-01-29 14:04:12 +0100

    działa *

--- application/models/Pracownik_model.php
+++ application/models/Pracownik_model.php
@@ -3,13 +3,13 @@
 class Pracownik_model extends CI_Model {
 
 	public function sprawdz_czy_kierownictwo($id_pracownika_placowki){
-        $this->db->select('kierownictwa.kierownik');
-        $this->db->from('kierownictwa');
-        $this->db->join('kierownicy', 'kierownicy.id = kierownictwa.kierownik');
-        $this->db->join('pracownicy_placowki', 'kierownicy.id = pracownicy_placowki.id');
-        $this->db->where('pracownicy_placowki.id', $id_pracownika_placowki);
-        $kierownictwo = $this->db->get();
-        return ($kierownictwo->num_rows() > 0);
+                $this->db->select('kierownictwa.kierownik');
+                $this->db->from('kierownictwa');
+                $this->db->join('kierownicy', 'kierownicy.id = kierownictwa.kierownik');
+                $this->db->join('pracownicy_placowki', 'kierownicy.id = pracownicy_placowki.id');
+                $this->db->where('pracownicy_placowki.id', $id_pracownika_placowki);
+                $kierownictwo = $this->db->get();
+                return ($kierownictwo->num_rows() > 0);
 	}
 }
 ?>
