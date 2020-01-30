5bc6971b7a8bc62ac2913157246115aba8380340
igahupalo <56322245+igahupalo@users.noreply.github.com>
2020-01-30 01:14:52 +0100

    kierownik na miejscu

--- application/models/Pracownik_model.php
+++ application/models/Pracownik_model.php
@@ -6,9 +6,10 @@ class Pracownik_model extends CI_Model {
                 $this->db->select('kierownictwa.kierownik');
                 $this->db->from('kierownictwa');
                 $this->db->join('kierownicy', 'kierownicy.id = kierownictwa.kierownik');
-                $this->db->join('pracownicy_placowki', 'kierownicy.id = pracownicy_placowki.id');
+                $this->db->join('pracownicy_placowki', 'kierownicy.pracownik_placowki = pracownicy_placowki.id');
                 $this->db->where('pracownicy_placowki.id', $id_pracownika_placowki);
                 $kierownictwo = $this->db->get();
+
                 return ($kierownictwo->num_rows() > 0);
 	}
 }
